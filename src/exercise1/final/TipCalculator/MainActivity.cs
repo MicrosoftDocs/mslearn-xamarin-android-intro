4233ec81c8546ab88986ad1807bee9dd4f9ee7f7
Adam Patridge <patridge@gmail.com>
2019-05-02 13:02:43 -0600

    Unify whitespace

--- src/exercise1/final/TipCalculator/MainActivity.cs
+++ src/exercise1/final/TipCalculator/MainActivity.cs
@@ -7,11 +7,14 @@ using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
-namespace TipCalculator {
+namespace TipCalculator
+{
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity {
+    public class MainActivity : AppCompatActivity
+    {
 
-        protected override void OnCreate(Bundle savedInstanceState) {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
@@ -23,14 +26,17 @@ namespace TipCalculator {
             fab.Click += FabOnClick;
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu) {
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
         }
 
-        public override bool OnOptionsItemSelected(IMenuItem item) {
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
             int id = item.ItemId;
-            if (id == Resource.Id.action_settings) {
+            if (id == Resource.Id.action_settings)
+            {
                 return true;
             }
 
@@ -42,7 +48,8 @@ namespace TipCalculator {
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- src/exercise2/final/TipCalculator/MainActivity.cs
+++ src/exercise2/final/TipCalculator/MainActivity.cs
@@ -7,11 +7,14 @@ using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 
-namespace TipCalculator {
+namespace TipCalculator
+{
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity {
+    public class MainActivity : AppCompatActivity
+    {
 
-        protected override void OnCreate(Bundle savedInstanceState) {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
@@ -20,22 +23,25 @@ namespace TipCalculator {
             SetSupportActionBar(toolbar);
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu) {
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
         }
 
-        public override bool OnOptionsItemSelected(IMenuItem item) {
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
             int id = item.ItemId;
-            if (id == Resource.Id.action_settings) {
+            if (id == Resource.Id.action_settings)
+            {
                 return true;
             }
 
             return base.OnOptionsItemSelected(item);
         }
 
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
